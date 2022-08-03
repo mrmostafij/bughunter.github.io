e06fa02db960a9e160ed44c78b3c79397112f4d9
mrmostafij <110459973+mrmostafij@users.noreply.github.com>
2022-08-03 06:47:14 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// BX Slider JS
+
+$(document).ready(function(){
+  $('.slider').bxSlider({
+    mode: 'fade',
+    auto: true,
+    stopAutoOnClick: true,
+    pager: true,
+    speed: 1500,
+  });
+  
+});
